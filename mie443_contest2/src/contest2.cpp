14e7e0b1df890bf17d8d672373a1d2907eab6e07
CTong7 <christong825@gmail.com>
2023-03-16 22:22:49 -0400

    debugging code

--- mie443_contest2/src/contest2.cpp
+++ mie443_contest2/src/contest2.cpp
@@ -1,8 +1,10 @@
 #include <boxes.h> //boxes.cpp is what loads gazebo_coords.xml into our contest file, we won't need to modify this EVER
 #include <navigation.h>
-#include <robot_pose.h>
+#include <robot_pose.h> // runs callback funciton to robot_pose.cpp, by claling a header you call a cpp???
 #include <imagePipeline.h>
 #include <chrono>
+#include <target_pose_calculation.cpp>
+#include <imagePipeline.cpp>
 
 int main(int argc, char** argv) {
     // Setup ROS.
@@ -12,7 +14,7 @@ int main(int argc, char** argv) {
     RobotPose robotPose(0,0,0); //RobotPose is a CLASS, we are creating an object called robotPose which takes in 3 inputs, x,y, yaw
     //robot pose is set to 0
 
-
+    //subscribe to amcl_pose which gives robot pose xyyaw, posecallback is what calls this 
     ros::Subscriber amclSub = n.subscribe("/amcl_pose", 1, &RobotPose::poseCallback, &robotPose);
 
     // Initialize box coordinates and templates
@@ -41,6 +43,42 @@ int main(int argc, char** argv) {
     // Execute strategy.
     while(ros::ok() && secondsElapsed <= 300) {
         ros::spinOnce(); //callback all subscribers
+
+        // Part 1: Calculate positions
+        //input: boxes.coords file
+        //output: NEW x,y,yaw coordinates
+        vector<float> pos1=target_pose(Boxes.coords[0]); //pass info of a single box
+        vector<float> pos2=target_pose(Boxes.coords[1]);//pass info of a single box
+        vector<float> pos3=target_pose(Boxes.coords[2]); //pass info of a single box
+        vector<float> pos4=target_pose(Boxes.coords[3]); //pass info of a single box
+        vector<float> pos5=target_pose(Boxes.coords[4]); //pass info of a single box
+
+        
+        //Part 2: Call travelling salesman
+        //input: 5 boxes.coords
+        //output: ordered list of positions to travel to, where start of list = 1st position to travel to
+        vector <float> ordered_locations={pos1,pos2,pos3,pos4,pos5}; //output 
+        std::cout<<ordered_locations<<""; // write out ordered lcoations
+
+
+        //Part 3: Call move_base
+        //input: ordered list of coordinates
+        //output 1: move to location and take photos
+        //ouput 2: text file of photo labels and lcoations
+        for (int i = 0; i<ordered_locations.size(); i++){
+            std::cout <<ordered_locations[i]<<" ";
+            //moveToGoal takes in Floats 
+            xGoal=ordered_locations[i].robot_x;
+            yGoal=ordered_locations[i].robot_y;
+            phiGoal=ordered_locations[i].robot_yaw;
+
+            bool Navigation::moveToGoal(xGoal,yGoal,phiGoal);
+
+            //Part 4: call image pipeline and take images
+            //******INSERT IMAGE PROCESSING CODE HERE****//
+        }
+
+      
 
         
         /***YOUR CODE HERE***/
--- mie443_contest2/src/target_pose_calculation.cpp
+++ mie443_contest2/src/target_pose_calculation.cpp
@@ -1,24 +1,27 @@
 #include <nav_msgs/GetPlan.h>
+#include <vector>
+using namespace std;
+
 // Define class for storing target pose of robot
 class target_robot_pose_class{
   public:
-     double robot_x, robot_y, robot_yaw;
+     float robot_x, robot_y, robot_yaw;
 };
 
-// Target Pose Calculation Function
-float target_object[] = boxes.coords[0]
-float phi = target_object[2]
 
-
-target_robot_pose target_pose(target_object_class target_object){
+//custom class called target_robot_pose
+//creating a function called target_pose
+//should just return a vector list of x,y and yaw position
+// INPUT = boxes.coords which is a vector float
+vector<float> target_pose(RobotPose robotPose, vector<float> target_object){
+    
 
     // Calculate point m_0 for a given object using the object's coordinates
-    m_0_x = target_object.object_x + m*cos(phi); // x-coordinate of point m_0
-    m_0_y = target_object.object_y + m*sin(phi); // y-coordinate of point m_0
+    float m_0_x = target_object.object_x + m*cos(phi); // x-coordinate of point m_0
+    float m_0_y = target_object.object_y + m*sin(phi); // y-coordinate of point m_0
 
-	// Store current robot x,y position
-	start.pose.position.x = robotPose.x;
-	start.pose.position.y = robotPose.y;
+
+
 
 	// Enter candidate goal position calculated above
 	goal.pose.position.x = m_i[i][1]; // input goal x coordinate 
@@ -26,10 +29,11 @@ target_robot_pose target_pose(target_object_class target_object){
 
 	// Check if point is valid
     ros::ServiceClient check_path = n.serviceClient<nav_msgs::GetPlan>("/move_base/NavfnROS/make_plan");
-    nav_msgs::GetPlan srv;
-    srv.request.start = start;
-    srv.request.goal = goal;
-    check_path.call(srv);
+    nav_msgs::GetPlan srv;
+    srv.request.start.pose.position.x=robotPose.x; // Get current position of robot
+    srv.request.start.pose.position.y=robotPose.y;// Get current position of robot
+    srv.request.goal = goal;
+    check_path.call(srv);
 
     if srv.response.plan.poses.size<0;{
          // Calculate point m_0 for a given object using the object's coordinates
@@ -41,12 +45,14 @@ target_robot_pose target_pose(target_object_class target_object){
 
         // Check if point is valid
         ros::ServiceClient check_path = n.serviceClient<nav_msgs::GetPlan>("/move_base/NavfnROS/make_plan");
-        nav_msgs::GetPlan srv;
-        srv.request.start = start;
-        srv.request.goal = goal;
-        check_path.call(srv);
+        nav_msgs::GetPlan srv;
+        srv.request.start.pose.position.x=robotPose.x; // Get current position of robot
+        srv.request.start.pose.position.y=robotPose.y; // Get current position of robot
+        srv.request.goal = goal;
+        check_path.call(srv);
     }
-    if {srv.response.plan.poses.size<0;{
+
+    if (srv.response.plan.poses.size<0){
          // Calculate point m_0 for a given object using the object's coordinates
         m_0_x = target_object.object_x + m*cos(phi-45); // x-coordinate of point m_0
         m_0_y = target_object.object_y + m*sin(phi-45); // y-coordinate of point m_0
@@ -56,32 +62,32 @@ target_robot_pose target_pose(target_object_class target_object){
 
         // Check if point is valid
         ros::ServiceClient check_path = n.serviceClient<nav_msgs::GetPlan>("/move_base/NavfnROS/make_plan");
-        nav_msgs::GetPlan srv;
-        srv.request.start = start;
-        srv.request.goal = goal;
-        check_path.call(srv);}
+        nav_msgs::GetPlan srv;
+        srv.request.start = start;
+        srv.request.goal = goal;
+        check_path.call(srv);
+    }
 
     int n = 8; // Specify number of possible search points around this specific m_0
-    double D = 0.5; // Specify search diameter around point m_0
-    double m_i[n][2]; // Array contianing search points around point m_0
+    float D = 0.5; // Specify search diameter around point m_0
+    float m_i[n][2]; // Array contianing search points around point m_0
 
     // Calculate search points around this specific m_0
     // Filter out inaccessible points (obstructed or off the map)
     for(i = 0; i < n; i++){
         m_i[i][1] = m_0_x + D*cos((2*M_PI)/(i+1)); // x-coord of m_i
-	m_i[i][2] = m_0_y + D*sin((2*M_PI)/(i+1)); // y-coord of m_i
+	    m_i[i][2] = m_0_y + D*sin((2*M_PI)/(i+1)); // y-coord of m_i
 
 	// Make request to make_plan service
-    ros::ServiceClient check_path = n.serviceClient<nav_msgs::GetPlan>("/move_base/NavfnROS/make_plan");
-    nav_msgs::GetPlan srv;
-    srv.request.start = start;
-    srv.request.goal = goal;
-    check_path.call(srv);
-
-    	// Return path reachability
+    ros::ServiceClient check_path = n.serviceClient<nav_msgs::GetPlan>("/move_base/NavfnROS/make_plan");
+    nav_msgs::GetPlan srv;
+    srv.request.start = start; // needs starting position as input
+    srv.request.goal = goal; //needs destination
+    check_path.call(srv);
+ 	// Return path reachability
 	// If "srv.response.plan.poses.size()" is > 0, a path can be obtained, so the statement evaluates to "true"
 	// If "srv.response.plan.poses.size()" is < 0, no path can be obtained, so the statement evaluates to "false"
-    	return srv.response.plan.poses.size() > 0;
+        return srv.response.plan.poses.size() > 0;
 
 	if(size() == true){
 	    // If point is valid, continue iterating through for loop
@@ -98,8 +104,8 @@ target_robot_pose target_pose(target_object_class target_object){
     // Calculate the centroid using the non-zero'd points in the m_i array 
 
     // Sum the x- and y- coords of all the search points
-    double sum_x = 0;
-    double sum_y = 0;
+    float sum_x = 0;
+    float sum_y = 0;
     for(i = 0; i < n; i++){
 	sum_x = sum_x + m_i[i][1];
         sum_y = sum_y + m_i[i][2];
@@ -110,9 +116,10 @@ target_robot_pose target_pose(target_object_class target_object){
     target_robot_pose.robot_y = sum_y/n;
     
     // Calculate desired robot orientation
-    double phi = boxes.coords[min_index][3]; // Object's orientation
+    float phi = boxes.coords[min_index][3]; // Object's orientation
     if(phi >= 0 && phi < M_PI){
     	target_robot_pose.robot_yaw = phi + M_PI;
+
     }
     else if(phi >= M_PI && phi < 2*M_PI){
         target_robot_pose.robot_yaw = phi - M_PI;
